9aed598e9939bce018dcc7aff9ca7f70427c4582
bforbhagya <bhagya.ariyarathne@gmail.com>
2023-11-17 11:27:07 +0530

    first commit

---
+++ quotes.js
@@ -0,0 +1,188 @@
+const localQuotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston S. Churchill",
+  },
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    author: "Albert Einstein",
+  },
+  {
+    quote:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+  },
+  { quote: "The purpose of our lives is to be happy.", author: "Dalai Lama" },
+  {
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  { quote: "Get busy living or get busy dying.", author: "Stephen King" },
+  {
+    quote:
+      "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.",
+    author: "Mark Twain",
+  },
+  { quote: "The best revenge is massive success.", author: "Frank Sinatra" },
+  {
+    quote: "The only impossible journey is the one you never begin.",
+    author: "Tony Robbins",
+  },
+  {
+    quote: "Life is really simple, but we insist on making it complicated.",
+    author: "Confucius",
+  },
+  {
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot",
+  },
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote:
+      "You have within you right now, everything you need to deal with whatever the world can throw at you.",
+    author: "Brian Tracy",
+  },
+  {
+    quote: "Don't count the days, make the days count.",
+    author: "Muhammad Ali",
+  },
+  {
+    quote:
+      "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+    author: "Jordan Belfort",
+  },
+  {
+    quote: "I find that the harder I work, the more luck I seem to have.",
+    author: "Thomas Jefferson",
+  },
+  {
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    quote:
+      "Do not wait to strike till the iron is hot, but make it hot by striking.",
+    author: "William Butler Yeats",
+  },
+  {
+    quote:
+      "The purpose of our lives is to add value to the people of this generation and those that follow.",
+    author: "Buckminster Fuller",
+  },
+  {
+    quote:
+      "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+    author: "Jordan Belfort",
+  },
+  {
+    quote:
+      "I am not a product of my circumstances. I am a product of my decisions.",
+    author: "Stephen R. Covey",
+  },
+  {
+    quote: "Life is 10% what happens to us and 90% how we react to it.",
+    author: "Charles R. Swindoll",
+  },
+  { quote: "The best revenge is massive success.", author: "Frank Sinatra" },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston S. Churchill",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote:
+      "Success is not how high you have climbed, but how you make a positive difference to the world.",
+    author: "Roy T. Bennett",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "Life is short, and it's up to you to make it sweet.",
+    author: "Sarah Louise Delany",
+  },
+  {
+    quote: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    quote:
+      "Success is liking yourself, liking what you do, and liking how you do it.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "The only way to achieve the impossible is to believe it is possible.",
+    author: "Charles Kingsleigh",
+  },
+  {
+    quote:
+      "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+    author: "Jordan Belfort",
+  },
+  {
+    quote:
+      "You can't go back and change the beginning, but you can start where you are and change the ending.",
+    author: "C.S. Lewis",
+  },
+  {
+    quote: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    quote: "Don't count the days, make the days count.",
+    author: "Muhammad Ali",
+  },
+];
